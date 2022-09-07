0807f5a1cf58909548eac90eac812cb874887f15
dliebel <diegoliebel@gmail.com>
2022-09-06 21:24:37 -0400

    diferentes botones

--- index.php
+++ index.php
@@ -10,7 +10,9 @@
         <input type="text" name="operador" >
         <label >valor 2</label>
         <input type="text" name="valor2" >
-        <button type="submit">enviar</button>
+        <button type="button" value="boton1">no envia</button>
+        <button type="submit" value="boton1">botton enviar</button>
+        <input type="submit" value="boton input"/>
       </form> 
     </body>
 </html>
